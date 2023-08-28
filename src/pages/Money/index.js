40159dc81216ed9994b030c2457204843a7ca2e0
marcellu-s <marcellusfran@outlook.com>
2023-08-28 14:10:45 -0300

    ATT:

--- src/pages/Money/index.js
+++ src/pages/Money/index.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { LinearGradient } from 'expo-linear-gradient';
-import { StyleSheet, Text, SafeAreaView, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, View, ScrollView } from 'react-native';
+
+import { Picker } from '@react-native-picker/picker';
+import { useState } from 'react';
 
 import Navbar from '../../components/Navbar';
 
 export default function Money() {
+
+    const [ selectedValue, setSelectedValue ] = useState('brl');
     
     return(
         <LinearGradient style={styles.container} colors={['#3A1B59','#D93D66']}>
@@ -13,9 +18,26 @@ export default function Money() {
 
                 <StatusBar style="light" />
 
-                <View>
-                    <Text>Money</Text>
-                </View>
+                <ScrollView style={{paddingHorizontal: 32}}>
+
+                    <Text style={styles.title}>CONVERSOR DE MOEDAS</Text>
+
+                    <View style={styles.topInput}>
+
+                        <View style={styles.picker}>
+                            <Picker style={{color: '#fff'}} selectedValue={selectedValue} onValueChange={(value) => {setSelectedValue(value)}}>
+                                <Picker.Item label="USD" value="usd" />
+                                <Picker.Item label="BRL" value="brl" />
+                            </Picker>
+                        </View>
+
+                        <View>
+                            <Text>120</Text>
+                        </View>
+
+                    </View>
+
+                </ScrollView>
 
             </SafeAreaView>
 
@@ -28,5 +50,23 @@ export default function Money() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+    },
+
+    title: {
+        color: '#fff',
+        fontSize: 24,
+        textAlign: 'center',
+        marginTop: 64,
+        fontWeight: '600'
+    },
+
+    picker: {
+        borderWidth: 1,
+        borderColor: '#fff',
+        borderRadius: 8
+    },
+
+    topInput: {
+        marginTop: 32
     },
 });
